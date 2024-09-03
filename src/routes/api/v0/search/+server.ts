5bf2663d1866eecf73e91b288e49b93adcc5a3c2
GearKite <emgh@em.id.lv>
2024-09-03 13:06:41 +0300

    Search API: add `order` option

--- src/routes/api/v0/search/+server.ts
+++ src/routes/api/v0/search/+server.ts
@@ -1,18 +1,26 @@
 // src/routes/api/search.ts
 import { json, type RequestHandler, error } from "@sveltejs/kit";
 import { db } from "$lib/server/db";
-import { and, ilike, eq, count, sql } from "drizzle-orm";
+import { and, ilike, eq, count, sql, desc, min, max } from "drizzle-orm";
 import { networks, observations } from "$lib/server/db/schema";
+import type { PgColumn, SQL } from "drizzle-orm/pg-core";
 
 export const GET: RequestHandler = async ({ url }) => {
   let bssid = url.searchParams.get("bssid");
   let ssid = url.searchParams.get("ssid");
   let networkType = url.searchParams.get("type");
   const page = parseInt(url.searchParams.get("page") || "0", 10);
+  let orderStr = url.searchParams.get("order");
 
   if (bssid === null || bssid === "") bssid = "%";
   if (ssid === null || ssid === "") ssid = "%";
   if (networkType === null || networkType === "") networkType = "W";
+
+  let order: PgColumn | SQL | SQL.Aliased = networks.id;
+  if (orderStr === "firstSeen") order = desc(min(observations.time));
+  if (orderStr === "lastSeen") order = desc(max(observations.time));
+  if (orderStr === "observationCount") order = desc(count(observations));
+  if (orderStr === "bssid") order = networks.bssid;
 
   const query = db
     .select({
@@ -36,6 +44,7 @@ export const GET: RequestHandler = async ({ url }) => {
       )
     )
     .groupBy(networks.id, observations.ssid)
+    .orderBy(order)
     .limit(100)
     .offset(page * 100);
 
